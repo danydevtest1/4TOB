5c8a274355153398ff9e9712ad9d34558c027b19
Dany <danycambrano1@gmail.com>
2025-01-28 16:00:26 -0600

    Se agregaron componentes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Operaciones from "./components/Operaciones";
+
+function App() {
+  
+
+  return (
+    <div className="app">
+     <Operaciones/>
+    </div>
+  );
+}
+
+export default App;
