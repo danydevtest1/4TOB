8ad030bc7f92541ae93919806956c4a8cb86ac9a
Dany <danycambrano1@gmail.com>
2025-01-30 17:13:30 -0600

    Camnbios en el prop

---
+++ src/components/productos/ItemProductos/ItemProductos.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+
+export function ItemProductos(props) {
+    const {nombre, descripcion}=props;
+
+  return (
+    <div>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>{nombre}</Card.Title>
+          <Card.Text>
+           {descripcion}
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
--- src/components/productos/ListProductos/ListProductos.js
+++ src/components/productos/ListProductos/ListProductos.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Stack } from "react-bootstrap";
+import { ItemProductos } from "../ItemProductos";
+import { Productos } from "../../../utils";
 
-export  function ListProductos() {
+export function ListProductos() {
   return (
-    <div>ListProductos</div>
-  )
+    <Stack gap={3}>
+      <div className="container p-2" style={{ textAlign: "center" }}>
+        <h1>Lista de productos</h1>
+        <div className="target">
+          {Productos.map((producto) => (
+            <ItemProductos
+              nombre={producto.nombre}
+              descripcion={producto.descripcion}
+            />
+          ))}
+        </div>
+      </div>
+    </Stack>
+  );
 }
-
